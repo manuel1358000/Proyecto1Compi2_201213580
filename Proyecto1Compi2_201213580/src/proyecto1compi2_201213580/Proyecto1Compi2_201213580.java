6ea2a5814037ac6994fea1c2743c2556c3584f5b
Manuel <manuel13580@gmail.com>
2019-02-13 21:28:53 -0600

    Creacion del Proyecto en Netbeans

---
+++ Proyecto1Compi2_201213580/src/proyecto1compi2_201213580/Proyecto1Compi2_201213580.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1compi2_201213580;
+
+/**
+ *
+ * @author anton
+ */
+public class Proyecto1Compi2_201213580 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
